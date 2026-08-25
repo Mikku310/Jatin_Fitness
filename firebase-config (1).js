8d2e3c60d26d43f57109a1db2b48682d94564154
Mikku310 <ja3maru@gmail.com>
2026-08-25 08:27:05 -0400

    Add files via upload

---
+++ firebase-config (1).js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCvzZbTVl9oMh9Mm5WRsugIX1lgvkybNPA",
+  authDomain: "jatin-fitness-tracker.firebaseapp.com",
+  projectId: "jatin-fitness-tracker",
+  storageBucket: "jatin-fitness-tracker.firebasestorage.app",
+  messagingSenderId: "816619496122",
+  appId: "1:816619496122:web:a1b10a1d0c97f5ef23d883"
+};
