6d426723b8530d075f86047be99d9b83d98346a5
Mikku310 <ja3maru@gmail.com>
2026-08-25 09:00:23 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyBN9ICUbzLWkQQx_vBRCkv-dHn2TC80E",
+const firebaseConfig = {
+  apiKey: "AIzaSyBN9ICbUzsLWKguX8_vBRGkv-dHm2TC80E",
   authDomain: "jatin-fitness-tracker-b6de4.firebaseapp.com",
   projectId: "jatin-fitness-tracker-b6de4",
   storageBucket: "jatin-fitness-tracker-b6de4.firebasestorage.app",
   messagingSenderId: "150575198043",
-  appId: "1:150575198043:web:fd7ac4199f15f3a39b47e"
+  appId: "1:150575198043:web:fd7ac4199f15f15a39b47e",
+  measurementId: "G-58Q1V3WH1W"
 };
