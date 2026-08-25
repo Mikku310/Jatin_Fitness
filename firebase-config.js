05eccd14d01c0af282d4bfda307014e15523831a
Mikku310 <ja3maru@gmail.com>
2026-08-25 09:25:10 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyBN9ICbUzsLWKguX8_vBRGkv-dHm2TC80E",
-  authDomain: "jatin-fitness-tracker-b6de4.firebaseapp.com",
-  projectId: "jatin-fitness-tracker-b6de4",
-  storageBucket: "jatin-fitness-tracker-b6de4.firebasestorage.app",
-  messagingSenderId: "150575198043",
-  appId: "1:150575198043:web:fd7ac4199f15f15a39b47e",
-  measurementId: "G-58Q1V3WH1W"
-};
